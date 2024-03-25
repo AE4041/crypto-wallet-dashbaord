edf7c489112f6f5d4ee5f6870e33f1a36fc9040a
Emmanuel <asamoahe304@gmail.com>
2024-03-25 00:40:22 +0000

    refactor dashboard flow

--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -1,23 +1,33 @@
+import { faker } from "@faker-js/faker"
 import { CirclesFour } from "@phosphor-icons/react"
 import Logo from "../../assets/img/Logo.png"
 import "./style.css"
 
 const Navbar = ({ closeState, closeHandler }) => {
+    const avatar = faker.image.avatarGitHub()
+    const userName = faker.person.firstName({ sex: 'male | female' })
 
     return (
-        <header className="header fixed top-0 left-0 w-full px-3">
-            <div className="header-container flex align-items-center justify-content-between ">
-                <div className="flex align-items-center gap-2">
+        <header className="header fixed top-0 left-0 flex align-items-center w-full px-3">
+            <div className="header-container flex align-items-center justify-content-between flex-1">
+                <div className="header-brand flex align-items-center gap-2">
                     <div className="logo-card">
                         <img src={Logo} alt="crypto logo" />
                     </div>
                     <h1 className="text-lg text-white">Crypto</h1>
                 </div>
 
-                <div className="header__toggle bg-bluegray-500 border-round-md p-2 flex align-items-center">
-                    <i onClick={() => closeHandler(!closeState)} className="text-white font-bold cursor-pointer">
-                        <CirclesFour size={20} />
-                    </i>
+                <div className="flex align-items-center gap-3">
+                    <div className="ml-auto flex gap-2 align-items-center">
+                        <p className="profile-name text-gray-50">{userName}</p>
+                        <img className="border-circle" src={avatar} alt="profile" style={{ width: "30px" }} />
+                    </div>
+
+                    <div className="header__toggle bg-bluegray-500 border-round-md p-2 flex align-items-center">
+                        <i onClick={() => closeHandler(!closeState)} className="text-white font-bold cursor-pointer">
+                            <CirclesFour size={20} />
+                        </i>
+                    </div>
                 </div>
             </div>
         </header>
--- src/pages/Dashboard/Dashbaord.jsx
+++ src/pages/Dashboard/Dashbaord.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react"
+import { faker } from "@faker-js/faker"
 import { Money, CurrencyEth, Wallet, AlignBottom } from "@phosphor-icons/react"
 import TransactionAssets from "./TransactionAssets"
 import { Transactions } from "../../data/Transactions"
 import Table from "../../components/Table/Table"
 import TransactionChart from "./TransactionChart"
-
 
 
 const Dashboard = () => {
@@ -15,22 +15,22 @@ const Dashboard = () => {
     }, [])
 
     return (
-        <div className="flex flex-column gap-4 mt-5 md:mt-2 lg:mt-2 fadein animation-duration-1000">
+        <div className="flex flex-column gap-4 mt-5 mb-9 md:mt-9 lg:mt-9 fadein animation-duration-1000">
             <div className="flex-1 flex flex-column md:flex-row lg:flex-row xl:flex-row gap-4 md:gap-0 lg:gap-0 w-full mx-auto">
                 <div className="flex-1 px-3">
                     <div className="flex flex-column gap-7 bg-blur p-5 border-round-xl">
                         <div className="flex flex-column text-gray-50 md:flex-row lg:flex-row gap-8 justify-content-start">
-                            {renderInfoBlock(Money, "Total assets", "$ 87.743")}
-                            {renderInfoBlock(Wallet, "Total deposits", "$ 78,342")}
-                            {renderInfoBlock(AlignBottom, "APY", "+ 12.3%")}
+                            {renderInfoBlock(Money, "Total assets")}
+                            {renderInfoBlock(Wallet, "Total deposits")}
+                            {renderInfoBlock(AlignBottom, "APY")}
                         </div>
 
-                        <div className="flex gap-3 align-items-center">
-                            <div className="flex gap-1 align-items-center bg-gray-800 px-3 py-2 border-round-3xl">
+                        <div className="flex gap-2 align-items-center">
+                            <div className="flex gap-1 align-items-center bg-gray-900 px-3 py-2 border-round-3xl">
                                 <CurrencyEth className="bg-white border-circle " size={25} color="#00000" weight="fill" />
                                 <p className="text-white text-sm">ETH</p>
                             </div>
-                            <p className="font-bold text-xl text-white">$ 78,342</p>
+                            <p className="font-bold text-xl text-white">{faker.finance.amount({ dec: 2, symbol: '$' })}</p>
                         </div>
 
                         <div className="flex w-full">
@@ -50,14 +50,14 @@ const Dashboard = () => {
     )
 }
 
-const renderInfoBlock = (IconComponent, label, value) => (
+const renderInfoBlock = (IconComponent, label) => (
     <div className="flex align-items-center gap-2">
         <div className="bg-blur-50 p-3 border-round-lg">
             <IconComponent size={25} />
         </div>
         <div className="flex flex-column gap-2">
             <p className="text-gray-200">{label}</p>
-            <p className="font-bold text-xl">{value}</p>
+            <p className="font-bold text-xl">{faker.finance.amount({ min: 400, max: 1000, dec: 2, symbol: '$' })}</p>
         </div>
     </div>
 );
